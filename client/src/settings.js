b52b543e1a8ce1946941a9782dfbdf0d7c8f4899
Nikesh Shrestha <nikesh@minerva.kgi.edu>
2020-04-30 15:23:44 -0700

    Cleaning settings files to default

--- client/src/settings.js
+++ client/src/settings.js
@@ -1,6 +1,6 @@
-export const OAUTH_CLIENT_ID = '49234056293-khtnao83br3kj6dvg3v109960uo62ic0.apps.googleusercontent.com';
-export const GRAPHQL_SERVER_URI = process.env.NODE_ENV === 'production'? '<Enter you production url>' :'http://localhost:4000/graphql';
-export const MAPBOX_API_ACCESS_TOKEN = 'pk.eyJ1IjoibmlrZXNoc2hyZXN0aGEiLCJhIjoiY2s4Z3k5Mjg5MDNlMTNrcXRsb204cjdjaCJ9.u_vgwT0owDEKCN9F5WXBFQ';
-export const UPLOAD_PRESET = 'geopins';
-export const CLOUD_NAME = 'nikeshshrestha';
+export const OAUTH_CLIENT_ID = '<OAUTH Cleing ID';
+export const GRAPHQL_SERVER_URI = process.env.NODE_ENV === 'production'? '<Enter you production url>' :'<Enter your local url for GraphQL server>';
+export const MAPBOX_API_ACCESS_TOKEN = '<Enter your Mapbox API token>';
+export const UPLOAD_PRESET = '<Enter your preset setting from Cloudinary>';
+export const CLOUD_NAME = '<Enter your preset setting from Cloudinary user name>';
 export const CLOUDINARY_API = 'https://api.cloudinary.com/v1_1'
